0199fed5b53d6f1d8620739579e77a55070ae496
overlord <rexatuzie@gmail.com>
2023-02-20 09:28:07 +0100

    mon commit 8

--- dev/import-dev-data.js
+++ dev/import-dev-data.js
@@ -32,4 +32,9 @@ const deleteData = async () => {
   }
 };
 
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
 console.log(process.argv);
