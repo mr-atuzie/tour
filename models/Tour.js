952d52d8b13f55a6b03db1faa320cb428936d39d
overlord <rexatuzie@gmail.com>
2023-02-20 07:15:13 +0100

    mon commit 1

--- models/Tour.js
+++ models/Tour.js
@@ -6,8 +6,52 @@ const tourSchema = new mongoose.Schema({
     required: [true, "Please enter tour name"],
     unique: true,
   },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, "Please enter tour price"] },
+  duration: {
+    type: Number,
+    required: [true, "Please enter tour duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "Please enter tour group size"],
+  },
+  difficulty: {
+    type: Number,
+    required: [true, "Please enter tour difficulty"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter tour price"],
+  },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "Please add tour description "],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "Please add tour cover Image "],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
